a703d37c55a1e5bee64f1f35fde242980e832aec
Varun Goyal <varungoyalkkp1@gmail.com>
2025-07-22 15:44:37 +0530

    Fixed UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
   };
 
   return (
-    <div className="navbar bg-base-300">
+    <div className="navbar bg-base-300  px-10">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">
           👩‍💻 DevTinder
